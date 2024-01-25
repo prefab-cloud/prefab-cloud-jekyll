a0318f1d2cc8a52e1923bb6d0c34fc7c5f160045
ayip8 <16192761+ayip8@users.noreply.github.com>
2024-01-25 15:17:17 -0500

    New variant syntax

--- lib/prefab/variant.rb
+++ lib/prefab/variant.rb
@@ -1,14 +1,33 @@
 module Jekyll
   class VariantTagBlock < PrefabBaseBlock
-    def initialize(tag_name, variant, tokens)
+    def initialize(tag_name, markup, tokens)
       super
-      @variant = variant.strip
+      if markup =~ /^\s*value:(\S+)\s+default:"([^"]+)"\s*$/i
+        # If the syntax is with a value and a default value
+        @value = $1.strip
+        @default = $2
+      elsif markup =~ /^\s*default:"([^"]+)"\s*$/i
+        # If the syntax is with just a default value
+        @default = $1
+        @value = nil
+      elsif markup.strip =~ /^\S+$/
+        # Basic config shorthand syntax
+        @value = markup.strip
+        @default = nil
+      else
+        # If syntax does not match any expected format, raise a syntax error
+        raise SyntaxError, "Syntax Error in 'variant' - Valid syntaxes: {% variant value:my-value default:\"some string\" %}, {% variant default:\"some string\" %}, or {% variant my-value %}"
+      end
     end
 
     def render(context)
       content = render_with_formatting(super, context)
 
-      "<div class=\"prefab-variant\" data-flag-name=\"#{context["flag_name"]}\" data-variant=\"#{@variant}\" style=\"display: none\">#{content}</div>"
+      if @default
+        "<div class=\"prefab-variant\" data-flag-name=\"#{context["flag_name"]}\" data-variant=\"#{@value}\">#{content}</div>"
+      else
+        "<div class=\"prefab-variant\" data-flag-name=\"#{context["flag_name"]}\" data-variant=\"#{@value}\" style=\"display: none\">#{content}</div>"
+      end
     end
 
   end
