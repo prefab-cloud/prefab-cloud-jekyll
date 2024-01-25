46fbeee31412d4bf6c7a8dbf952323cadeb4e224
ayip8 <16192761+ayip8@users.noreply.github.com>
2024-01-25 14:50:40 -0500

    After evaluation callback, config syntax

--- lib/prefab-cloud-jekyll.rb
+++ lib/prefab-cloud-jekyll.rb
@@ -4,6 +4,7 @@ require 'prefab/base_block'
 require 'prefab/feature_flag'
 require 'prefab/variant_default'
 require 'prefab/variant'
+require 'prefab/config'
 
 def snake_to_camel(obj)
   case obj
@@ -20,7 +21,11 @@ def snake_to_camel(obj)
 end
 
 Jekyll::Hooks.register :pages, :post_render do |page|
-  config = snake_to_camel(page.site.config['prefab'])
+  config = page.site.config['prefab']
+  if config['after_evaluation_callback']
+    file = File.join(page.site.source, config['after_evaluation_callback'])
+    callback = File.read(file)
+  end
 
   source_file = File.expand_path('../prefab.js', __FILE__)
   js_content = File.read(source_file)
@@ -32,8 +37,10 @@ Jekyll::Hooks.register :pages, :post_render do |page|
       type=\"text/javascript\"
     ></script>
     <script type=\"text/javascript\">
-      const config = #{config.to_json};
+      const config = #{snake_to_camel(config).to_json};
       console.log(config);
+
+      afterEvaluationCallback = (key, value) => {#{callback}};
 
       #{js_content}
     </script>
@@ -45,3 +52,4 @@ end
 Liquid::Template.register_tag('feature_flag', Jekyll::FeatureFlagTagBlock)
 Liquid::Template.register_tag('variant', Jekyll::VariantTagBlock)
 Liquid::Template.register_tag('variant_default', Jekyll::VariantDefaultTagBlock)
+Liquid::Template.register_tag('config', Jekyll::ConfigTag)
---
+++ lib/prefab/config.rb
@@ -0,0 +1,30 @@
+module Jekyll
+  class ConfigTag < Liquid::Tag
+
+    def initialize(tag_name, markup, tokens)
+      super
+      if markup =~ /^\s*key:(\S+)\s+default:"([^"]+)"\s*$/i
+        # If the syntax is with a default value
+        @key = $1.strip
+        @default = $2
+      elsif markup.strip =~ /^\S+$/
+        # Basic config shorthand syntax
+        @key = markup.strip
+        @default = nil
+      else
+        # If syntax does not match either format, raise a syntax error
+        raise SyntaxError, "Syntax Error in 'config' - Valid syntax: {% config key:my-config default:\"some string\" %} or {% config my-config %}"
+      end
+    end
+
+    def render(context)
+      if @default
+        # Render with default value inside the span tag
+        "<span data-config-name=\"#{@key}\">#{@default}</span>"
+      else
+        # Render only the key as data attribute
+        "<span data-config-name=\"#{@key}\"></span>"
+      end
+    end
+  end
+end
